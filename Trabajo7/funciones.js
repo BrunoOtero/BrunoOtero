02b1ba94aaace753ae649e28e35315005b6d6ad2
Bruno Otero <brunoomg10@gmail.com>
2022-05-16 11:02:19 -0300

    Correccion del Trabajo 7

--- Trabajo7/funciones.js
+++ Trabajo7/funciones.js
@@ -3,12 +3,21 @@ fetch(`https://randomuser.me/api`)
 .then(data=>{
     console.log(data)})
 
-function Ver(){
+
+const btnSolicitar = document.getElementById("btn")
+
+    function Ver(){
     fetch(`https://randomuser.me/api`)
     .then(response=>response.json())
     .then(data=>{
-        document.getElementById("A").data.name.first
-
+        document.getElementById("A").innerHTML = data.results[0].name.first
+        document.getElementById("B").innerHTML = data.results[0].name.last
+        document.getElementById("C").innerHTML = data.results[0].gender
+        document.getElementById("D").innerHTML = data.results[0].dob.age
+        document.getElementById("E").innerHTML = data.results[0].email
+        document.getElementById("FOTO").src = data.results[0].picture.large
     })
         
 }
+
+addEventListener(`click`, () => Ver(btnSolicitar))
