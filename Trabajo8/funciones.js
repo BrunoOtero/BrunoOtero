60feba13ca0b46428ca8dbb0603aeb19c3e5d991
Bruno Otero <brunoomg10@gmail.com>
2022-05-20 19:19:59 -0300

    Mapa Trabajo 8

--- Trabajo8/funciones.js
+++ Trabajo8/funciones.js
@@ -14,12 +14,24 @@ const btnBuscar = document.getElementById("btn")
                     Email: ${data.results[0].email} <br>
                     Latitud: ${data.results[0].location.coordinates.latitude}<br>
                     Longitud: ${data.results[0].location.coordinates.longitude}<br>`
-        
-            let NOEXIST = `NO EXISTE EL USUARIO/A`    
+                    
+                  
+            
+                    let NOEXIST = `NO EXISTE EL USUARIO/A`    
         
         if (data.results[0].gender == "female" && data.results[0].dob.age <= 40 && data.results[0].dob.age >= 20 ){
                 document.getElementById("contenido").innerHTML = HTML
                 document.getElementById("FOTO").src = data.results[0].picture.large
+
+                var map = L.map('map').setView([data.results[0].location.coordinates.latitude, data.results[0].location.coordinates.longitude], 13);
+
+    
+                L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+                
+                }).addTo(map);
+
+                L.marker([data.results[0].location.coordinates.latitude, data.results[0].location.coordinates.longitude],{draggable: true}).addTo(map);
             }
         else {
         
@@ -29,10 +41,7 @@ const btnBuscar = document.getElementById("btn")
     })
         
 }
-var mapa = new L.map('map', {
-    center: [20, 50],
-    zoom: 2    
-});
+
 
             
             
